2eb546bcd09c6722f701ef459c8e279dd5e7ec5d
alexei guevara <alexguev@gmail.com>
2012-01-14 21:27:59 -0500

    added a bit of mockig, made all unit tests pass

--- rest/test/garbageday/test/rest.clj
+++ rest/test/garbageday/test/rest.clj
@@ -9,15 +9,14 @@
 ;re-write to test each individual element. test existence and absense
 (deftest test-routes
   (is (= 200 (:status (request "/api/1.0/lg=1.1&lt=2.2" routes))) "return status 200-OK")
-  (is (= "{\"day-of-week\":\"Monday\",\"lg\":\"1.1\",\"lt\":\"2.2\"}"
-         (:body (request "/api/1.0/lg=1.1&lt=2.2" routes))) "match url and parameters")
-  (is (= "{\"day-of-week\":\"Monday\",\"lg\":\"-1.1\",\"lt\":\"-2.2\"}"
-         (:body (request "/api/1.0/lg=-1.1&lt=-2.2" routes))) "match url and negative parameters")
+  (with-redefs [garbageday.model/garbage-day (fn [lg lt] "Monday")]
+    (is (= "{\"day-of-week\":\"Monday\"}" (:body (request "/api/1.0/lg=1.1&lt=2.2" routes))) "match url and parameters"))
+  (with-redefs [garbageday.model/garbage-day (fn [lg lt] "Monday")]
+    (is (= "{\"day-of-week\":\"Monday\"}" (:body (request "/api/1.0/lg=-1.1&lt=-2.2" routes))) "match url and negative parameters"))
   (is (nil? (request "/" routes)) "no query and no parameters")
   (is (nil? (request "/api/1.0/lg=11&lt=2.2" routes)) "longitude missing decimal")
   (is (nil? (request "/api/1.0/lg=1.1&lt=22" routes)) "latitude missing decimal")
   (is (nil? (request "/other/1.0/lg=11&lt=2.2" routes)) "miss-matched query")
   (is (nil? (request "/api/x.x/lg=11&lt=2.2" routes)) "miss-matched version")
   )
-
 
